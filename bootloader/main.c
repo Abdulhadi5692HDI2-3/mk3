61d19f2d964a0bac043d8b491e948ad4ad67f8d5
abdulhadi5692hdi2 <randomemail>
2024-04-27 12:20:23 +0100

    .

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,6 +1,16 @@
 #include <uefi.h>
 
+char* kernelPath = "\\kernel.elf";
+
 int main(int argc, char** argv) {
+	(void)argc;
+	(void)argv;
+	if (argc > 0) {
+		kernelPath = argv[1];
+		printf("custom kernel path passed on cmdline. %s selected instead of \\\\kernel.elf", kernelPath);
+	}
+	FILE* kernel;
+
 	printf("Hello world!\n");
 	return 0;
 }
